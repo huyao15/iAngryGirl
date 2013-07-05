cb764d9ebf57be3dc8873fe6dffec59127d5bb22
huyao <huyao@meituan.com>
2013-07-05 19:52:52 +0800

    Initial Commit

---
+++ AngryGirl/XLQAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  XLQAppDelegate.h
+//  AngryGirl
+//
+//  Created by 胡尧 on 13-7-5.
+//  Copyright (c) 2013年 胡尧. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface XLQAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
